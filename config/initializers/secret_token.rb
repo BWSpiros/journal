27762a32a77edc19c6b4eca7f4b27dc62bdcc24c
David Park & Brian Stearns <david+brian@appacademy.io>
2013-12-05 09:11:41 -0500

    Initial commit: create Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Journal::Application.config.secret_token = '5b97d214b14f5e5681b7088eec466514418c6fbc7a565748df9747dcaeb884a02657dcec8564efca8d2e7776d4ded95d90ad1a7938be80aaf40e13edf882c96a'
